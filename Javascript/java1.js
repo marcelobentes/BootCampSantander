d105b0898ba2132b48153edb8dec3a9de309bef0
marcelobentes <marcelo.maia.m@gmail.com>
2022-07-21 18:14:09 -0300

    adicionando arquivo java1

---
+++ Javascript/java1.js
@@ -0,0 +1,15 @@
+console.log("Vamos la !!!");
+
+function par(array){
+    let numeros = [];
+    for (let i = 0; i < array.length; i++ ){
+        if(array[i] % 2 === 0){
+            numeros.push(array[i]);
+        }
+    }
+    console.log ('Os numeros pares: ',numeros);
+}
+
+let array = [1, 2, 3, 4, 5, 6, 7 ,8 ,9 ,10];
+
+par(array);
