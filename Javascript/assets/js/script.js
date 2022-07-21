9559bdd9e112ae0849dfebb281fd830f058423ed
marcelobentes <marcelo.maia.m@gmail.com>
2022-07-21 19:43:43 -0300

    adicionando um contador simples

---
+++ Javascript/assets/js/script.js
@@ -0,0 +1,12 @@
+console.log("Vamos lá no Script !!");
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+function decrement(){
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function increment(){
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
