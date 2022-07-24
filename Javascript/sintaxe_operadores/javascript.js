8309460c3a9fe262ca36a9dda843353e93be6b9d
marcelobentes <74937617+marcelobentes@users.noreply.github.com>
2022-07-23 21:17:15 -0300

    sintaxe e operadores

---
+++ Javascript/sintaxe_operadores/javascript.js
@@ -0,0 +1,17 @@
+function compararNumeros (num1, num2){
+    var soma = num1 + num2;
+    if (num1 === num2){
+        console.log("Os numeros ",num1,"e",num2," são iguais");
+        if(num1 != num2){
+            console.log("Os numeros ",num1,"e",num2," não são iguais");
+        }
+        
+    }
+    else if (soma > 10 && soma < 20 ){
+        console.log("Sua soma é ",soma, " que é maior que 10 e menor que 20");
+    }
+    else console.log("Sua soma é ",soma," que é menor que 10 e menor que 20");
+
+}
+
+compararNumeros(10,2);
