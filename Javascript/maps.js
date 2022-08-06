d2d3dc66bd3bcb3f393771b3b66d2b8631dea9cb
marcelobentes <marcelo.maia.m@gmail.com>
2022-08-06 09:19:20 -0300

    filter

--- Javascript/maps.js
+++ Javascript/maps.js
@@ -1,4 +1,5 @@
 const myMap = new Map();
+const array = [1, 2, 3, 4, 5, 6, 7 ,8]
 
 myMap.set('Marcelo', 'Admin');
 myMap.set('Ricardo0', 'Membro');
@@ -18,4 +19,36 @@ function getAdmins (map){
 
 }
 
+function filter (array){ 
+    let pares = [];
+    for(let i = 0; i < array.length; i++){
+        if (array[i] % 2 === 0){
+            pares.push(array[i]);
+        }
+
+    }
+
+    return pares;
+
+}
+
+//usando filter
+function filterPares (array){ 
+    return array.filter(callback);
+}
+
+function callback(item){
+    return item % 2 === 0;
+}
+
+
+const novoarray = array.map((item) => item *2); //usando o map
+
 console.log(getAdmins(myMap));
+
+console.log(array.map((item) => item *2));
+console.log(novoarray);
+console.log(`primeiro array ${array}`)
+
+console.log(filter(array));
+console.log(filterPares(array)); //usando filter
